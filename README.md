530580c0e98138952ae047fd490748e2bce6eb76
jimmyitis <dimitrovdime@outlook.com>
2018-04-04 18:14:11 +0200

    newest update
    Update the latest version

---
+++ README.md
@@ -0,0 +1,2 @@
+#
+Welcome to my GPIO app
---
+++ client/app.js
@@ -0,0 +1,23 @@
+var socket = require("socket.io-client")("http://localhost:3000");
+var gpio = require("rpi-gpio");
+
+process.on("SIGINT", function(){
+  gpio.write(12, false, function(){
+    gpio.destroy(function(){
+      process.exit();
+
+    });
+  });
+});
+
+gpio.setup(12, gpio.DIR_OUT, function(){
+  gpio.write(12, false); //turns LED off
+});
+
+socket.on("connect", function(){
+  console.log("Connected to server");
+  socket.on("updateState", function(state){
+    console.log("The new state is: " + state);
+    gpio.write(12, state);
+  });
+});
